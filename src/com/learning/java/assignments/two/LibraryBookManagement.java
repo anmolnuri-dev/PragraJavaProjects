dda0d7d5d0694ac29ffeb9b67c62c2f9ad34ddab
anmol nuri <anmol.nuri10@gmail.com>
2024-10-18 14:53:55 -0400

    added a main method to test the Library Book Management class

--- src/com/learning/java/assignments/two/LibraryBookManagement.java
+++ src/com/learning/java/assignments/two/LibraryBookManagement.java
@@ -1,5 +1,7 @@
 package com.learning.java.assignments.two;
 
+import java.util.Scanner;
+
 public class LibraryBookManagement {
     int numOfItems = 6;
     String [] bookArr;
@@ -43,10 +45,38 @@ public class LibraryBookManagement {
     }
 
     public void display(){
-        System.out.println("Current Library Inventory");
+        System.out.println("Current Library Inventory:");
         for( String s : bookArr) {
             System.out.println(s);
         }
+    }
+
+    public static void main(String[] args) {
+        String [] books = {"Game Of Thrones", "The Alchemist", "Harry Potter", "Crime and Punishment","Sapiens","Atomic Habits"};
+        LibraryBookManagement mountPleasant = new LibraryBookManagement(books);
+
+        //Searches if the book user inputted exists
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the name of a book to search: " );
+        String search = sc.nextLine();
+
+        mountPleasant.searchBook(search);
+
+        //Display the list of books in the library
+        mountPleasant.display();
+
+        System.out.print("Enter the name of the book you want to replace: " );
+        String existing = sc.next();
+        System.out.print("Enter the name of the book you want to replace: " );
+        String replace = sc.next();
+
+        //Replacing an existing book with a new book.
+        mountPleasant.updateInventory(existing, replace);
+
+        //Display the list of books after updating the inventory.
+        mountPleasant.display();
+
+        sc.close();
     }
 
 }
